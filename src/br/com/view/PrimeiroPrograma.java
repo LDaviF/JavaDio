3ca971c1e9e85972b9aa983fd63242aff4e57a7d
Davi <luisdaviferreira@gmail.com>
2022-10-04 19:50:09 -0300

    First commit

---
+++ src/br/com/view/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.view;
+
+import br.com.view.model.cat;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        cat gato = new cat();
+
+        System.out.println(gato);
+
+      /*  int a = 5;
+        int b = 45;
+        System.out.print("Hello World " + (a + b));*/
+    }
+}
